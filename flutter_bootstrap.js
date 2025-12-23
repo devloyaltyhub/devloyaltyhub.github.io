4a7eedc4c6613d1dd6317e0e09ffc3e230f8dd74
Victor Proppe <vctudo.27@gmail.com>
2025-12-23 18:27:36 -0300

    debug build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1676030385"
+    serviceWorkerVersion: "2062704047"
   }
 });
