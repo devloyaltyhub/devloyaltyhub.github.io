129b038f66403b4384333e373e0bcf5a6b3b6562
Victor Proppe <devloyaltyhub@gmail.com>
2026-03-09 00:55:55 -0300

    Deploy Admin Web v0.0.1+8 - 2026-03-09

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -41,7 +41,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c3b267dfafb4e5c5089d27ab33ffcf16
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "381f9f59229500eb08bf71722bc6fe51",
+"flutter_bootstrap.js": "e619fd3876bf78c31e9401eb868a0f26",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
