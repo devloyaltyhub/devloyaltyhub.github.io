5781ef2f3ae930fe45f3340e284d87f471110e7e
Victor Proppe <devloyaltyhub@gmail.com>
2026-03-09 00:59:31 -0300

    Deploy Admin Web v0.0.1+8 - 2026-03-09

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "522211811"
+    serviceWorkerVersion: "3973963506"
   }
 });
